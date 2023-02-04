78508d77784dc3934d77e40b2ccb4570e587ff4a
Lera <108754223+Linkshegelianer@users.noreply.github.com>
2023-02-04 20:25:28 +0300

    add swtich/case to main class

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,8 +1,27 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
 	public static void main(String[] args) {
-		Cli.greetUser();
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Please enter the game number and press Enter.");
+		System.out.println("1 - Greet");
+		System.out.println("0 - Exit");
+		String number = scanner.next();
+		System.out.println("Your choice: " + number);
+
+		switch (number) {
+			case "1":
+				Cli.greetUser();
+				break;
+			case "2":
+				System.out.println("The feature is not done yet!");
+				break;
+			default:
+				System.out.println("Incorrect number!");
+				break;
+		}
 	}
 }
 
