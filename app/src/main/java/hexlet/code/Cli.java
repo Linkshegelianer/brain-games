fa91e9dbc08a17fdd1fb50d96610524692bc5c2f
Lera <108754223+Linkshegelianer@users.noreply.github.com>
2023-02-04 16:48:20 +0300

    add Cli.class and Makefile for func

---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,15 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Cli {
+    public static void greetUser() {
+        System.out.println("Welcome to the Brain Games!");
+        System.out.println("May I have your name? ");
+
+        Scanner scanner = new Scanner(System.in);
+        String userName = scanner.next();
+
+        System.out.println("Hello, " + userName + "!");
+    }
+}
